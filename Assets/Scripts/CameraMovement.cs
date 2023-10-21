7b2d1fb1412c82352126a561c6aa1c069fe116d2
Murilo André <muriloandre@bitbucket.org>
2023-10-20 22:43:19 -0300

    Try: Changed Camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,8 +6,13 @@ public class CameraMovement : MonoBehaviour
     public Transform target; // Reference to the character's transform
     public Vector3 offset = new Vector3(0f, 14f, 0f); // Offset from the character's position
 
-    public float smoothSpeed = 1f; // Speed at which the camera follows the character
+    public float smoothSpeed = 3f; // Speed at which the camera follows the character
     public float rotationSpeed = 0f; // Speed at which the camera rotates
+
+    void Start(){
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = desiredPosition;
+    }
 
     private void LateUpdate()
     {
@@ -21,9 +26,9 @@ public class CameraMovement : MonoBehaviour
         //Quaternion desiredRotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
 
         // Smoothly move the camera towards the desired position and rotation
-        //transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         //transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
         
-        transform.position = desiredPosition;
+        //transform.position = desiredPosition;
     }
 }
